cae442d15134ef6bef9974fc0dd4e38a4564cab1
最终变身的小黑 <32385101+shidifenniya@users.noreply.github.com>
2018-02-03 20:30:02 +0800

    代码优化精简
    测试Windows下提交Git

--- src/main/java/com/teamRocket/controller/login/LoginController.java
+++ src/main/java/com/teamRocket/controller/login/LoginController.java
@@ -1,18 +1,13 @@
 package com.teamRocket.controller.login;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by dllo on 18/2/3.
  */
+@Controller
 public class LoginController  {
-    /* 用于测试项目是否能进入启动页 */
-//    @RequestMapping(value = {"","/"})
-//    public String home(){
-//
-//        return "login/login";
-//
-//    }
+
+
 
 }
