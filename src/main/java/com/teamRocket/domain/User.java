49c6127b114587afc28fb1e5f2880d945412a8d2
最终变身的小黑 <32385101+shidifenniya@users.noreply.github.com>
2018-02-05 11:57:03 +0800

    数据测试完成
    封装Mapper
    数据库连接写入数据测试完成
    新建工具类

---
+++ src/main/java/com/teamRocket/domain/User.java
@@ -0,0 +1,84 @@
+package com.teamRocket.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+
+    private String userId;
+
+    private String username;
+
+    private String email;
+
+    private String loginName;
+
+    private String password;
+
+    public User() {
+
+    }
+
+    public User(String userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public User(String userId, String username, String email, String loginName, String password) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.loginName = loginName;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", loginName='" + loginName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/teamRocket/mapper/login/UserDao.java
@@ -0,0 +1,10 @@
+package com.teamRocket.mapper.login;
+
+import com.teamRocket.domain.User;
+import com.teamRocket.mapper.BaseDao;
+
+
+public interface UserDao extends BaseDao<User> {
+
+
+}
